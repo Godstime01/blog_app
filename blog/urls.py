d3b785d312c3fe14b921b7af99f9081c20214d40
Godstime01 <youngestkid01@gmail.com>
2021-04-22 22:23:34 -0700

    add forms and user accounts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import HomeListView, HomeDetailView
+from .views import (
+    HomeListView,
+    HomeDetailView,
+    HomeCreateView,
+    HomeUpdateView,
+    HomeDeleteView,
+)
 
-urlpatterns =[
-    path('', HomeListView.as_view(), name = 'index'),
-    path('post/<int:pk>/', HomeDetailView.as_view(), name='post-detail')
+urlpatterns = [
+    path("", HomeListView.as_view(), name="index"),
+    path("post/<int:pk>/", HomeDetailView.as_view(), name="post-detail"),
+    path("post/new/", HomeCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/edit", HomeUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete", HomeDeleteView.as_view(), name="post_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
 class HomeListView(ListView):
-    model=Post
-    template_name='index.html'
-    
+    model = Post
+    template_name = "index.html"
+
+
 class HomeDetailView(DetailView):
     model = Post
-    template_name = 'post-detail.html'
+    template_name = "post-detail.html"
+
+
+class HomeCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class HomeUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class HomeDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("index")
